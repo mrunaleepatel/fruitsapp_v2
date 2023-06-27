8b368aca7ae9f75898245b8acc2aca106eb933af
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-27 18:48:37 -0500

    working

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-mongoose.connection(DATABASE_URL);
+mongoose.connect(DATABASE_URL);
 
 mongoose.connection
     .on('open', () => console.log('mongoose is connected'))
