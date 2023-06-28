3260102a902ed2a0feb8a52c2c8bd08452cd7aca
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-27 19:39:33 -0500

    updated

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const Fruit = require('./models/fruit');
+
+
+app.get('/fruit', async(req, res) => {
+    const allFruits = await Fruit.find({})
+
+    res.render(
+        'fruits/index.ejs',
+        { fruits: allFruits }
+
+        // the above the same thing as 
+        // {
+        //     fruits: [{ name: 'banana' }, { name: 'apple' }] 
+        // }
+        // or 
+        // is the same thing as 
+        // {
+        //     fruits: fruits 
+        // }
+    )
+});
+
+
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
